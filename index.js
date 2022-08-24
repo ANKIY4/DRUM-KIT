7b9d6952b90d3cb3a3e36460d2cde256a94ced60
ANKIY4 <107880014+ANKIY4@users.noreply.github.com>
2022-08-24 22:57:52 +0545

    its a drum kit.

---
+++ index.js
@@ -0,0 +1,48 @@
+//for clicking.
+for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    makeSound(this.innerHTML);
+    animation(this.innerHTML);
+  });
+}
+//for button press.
+document.addEventListener("keypress", function() {
+  makeSound(event.key);
+  animation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "j":
+      new Audio("sounds/snare.mp3").play();
+      break;
+    case "k":
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "l":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+    default:
+
+  }
+}
+
+function animation(getkey) {
+  document.querySelector("." + getkey).classList.add("pressed");
+
+  setTimeout(function() {
+    document.querySelector("." + getkey).classList.remove("pressed");
+  }, 100);
+}
